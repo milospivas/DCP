557ce64b41fd46d66a9d2945c4f219ca40e37a9e
Miloš Pivaš <milos.pivas@gmail.com>
2020-07-24 17:33:38 +0200

    major clean-up Refactored the solution into a class External function solution() calls the class method Solution.solution().

--- codility/Lesson04/MaxCounters.py
+++ codility/Lesson04/MaxCounters.py
@@ -2,6 +2,72 @@
 # you can write to stdout for debugging purposes, e.g.
 # print("this is a debug message")
 
+class Solution:
+
+    class Node:
+
+        def __init__(self, val = 0, time = 0):
+            self.val = val
+            self.time = time
+
+    class Counters:
+
+        def __init__(self, N):
+            self.N = N
+            self.counter = {}
+            self.min = 0
+            self.min_time = 0
+            self.max = 0
+
+        def increment(self, i, time):
+            if not i in self.counter:
+                self.counter[i] = Solution.Node()
+
+            node = self.counter[i]
+
+            new_val = (self.min + 1) if (node.time < self.min_time) else (node.val + 1)
+
+            node.val = new_val
+            node.time = time
+
+            if new_val > self.max:
+                self.max = new_val
+
+
+        def set_min_to_max(self, time):
+            self.min = self.max
+            self.min_time = time
+
+        def get_all_counters(self):
+            counters = [0 for _ in range(self.N)]
+
+            for i in range(1, self.N+1):
+                counters[i-1] = (self.counter[i].val) if ((i in self.counter) and (self.counter[i].time > self.min_time)) else self.min
+
+            return counters
+
+
+    @classmethod
+    def solution(cls, N, A):
+
+        if N == 0:
+            return []
+        if A == []:
+            return [0] * N
+
+
+        C = cls.Counters(N)
+        for time, i in enumerate(A):
+            if i < N+1:
+                C.increment(i, time+1)
+            else:
+                C.set_min_to_max(time+1)
+
+        counters = C.get_all_counters()
+
+        return counters
+
+
 def solution(N, A):
     ''' Returns a list representing the final states of N counters
     after operations given by A.
@@ -24,96 +90,6 @@ def solution(N, A):
         with operations given by A.
     '''
 
-    class Node:
+    sol = Solution.solution(N, A)
 
-        def __init__(self, val = 0, time = 0):
-            self.val = val
-            self.time = time
-
-    class Counters:
-
-        def __init__(self, N):
-            self.N = N
-            self.counter = {}
-            self.min = 0
-            self.min_time = 0
-            self.max = 0
-
-        def increment(self, i, time):
-            if not i in self.counter:
-                self.counter[i] = Node()
-
-            node = self.counter[i]
-
-            new_val = (self.min + 1) if (node.time < self.min_time) else (node.val + 1)
-
-            node.val = new_val
-            node.time = time
-
-            if new_val > self.max:
-                self.max = new_val
-
-
-        def set_min_to_max(self, time):
-            self.min = self.max
-            self.min_time = time
-
-        def get_all_counters(self):
-            counters = [0 for _ in range(N)]
-
-            for i in range(1, N+1):
-                counters[i-1] = (self.counter[i].val) if ((i in self.counter) and (self.counter[i].time > self.min_time)) else self.min
-
-            return counters
-
-
-    def test():
-        n = Node(42, 0)
-        print(n.val, n.time)
-
-        C = Counters(N)
-        print(C.N, C.counter, C.max)
-
-        i, time = 1, 1
-        C.increment(i, time)
-        print(C.counter, C.counter[i].val, C.counter[i].time)
-        print(C.min, C.min_time, C.max)
-
-        C.set_min_to_max(4)
-        print(C.min, C.min_time, C.max)
-
-        i, time = 2, 5
-        C.increment(i, time)
-        print(C.counter, C.counter[i].val, C.counter[i].time)
-        print(C.min, C.min_time, C.max)
-
-        counters = C.get_all_counters()
-        print(counters)
-
-        i, time = 2, 6
-        C.increment(i, time)
-        counters = C.get_all_counters()
-        print(counters)
-
-        time = 7
-        C.set_min_to_max(time)
-        counters = C.get_all_counters()
-        print(counters)
-
-        C = Counters(1)
-        C.increment(1, 1)
-        print(C.counter[1].val, C.counter[1].time)
-        counters = C.get_all_counters()
-        print(counters)
-
-
-    C = Counters(N)
-    for time, i in enumerate(A):
-        if i < N+1:
-            C.increment(i, time+1)
-        else:
-            C.set_min_to_max(time+1)
-
-    counters = C.get_all_counters()
-
-    return counters
+    return sol
